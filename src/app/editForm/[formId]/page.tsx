caadf15a94290536a699804a40290bf41f19f538
Asish <balajiashish02@gmail.com>
2025-02-23 13:42:52 +0530

    fixes

--- src/app/editForm/[formId]/page.tsx
+++ src/app/editForm/[formId]/page.tsx
@@ -1,7 +1,24 @@
-export function EditForm({ params }: { params: { formId: string } }) {
-  const id = params.formId;
-  console.log(id);
-  return<div>
-    form is {id}
-  </div>
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function EditForm({
+  params,
+}: {
+  params: { formId: string };
+}) {
+  const {formId} = await params;
+  const id = Number(formId);
+
+  const result = await prisma.form.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  const ans = JSON.parse(result?.content as string);
+  console.log(ans)
+  console.log(ans.formTitle);
+  console.log(ans.formSubHeading);
+  console.log(ans.formFields);
+  return <div>form is </div>;
 }
